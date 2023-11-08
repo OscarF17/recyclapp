962d8a43ef1991ec0e57ef6562037e4fc421a284
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-11-08 03:28:50 -0600

    add bottom navbar and sections

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -3,41 +3,39 @@ package com.example.proyectofinal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.proyectofinal.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Camera())
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.bottom_camera -> replaceFragment(Camera())
+                R.id.bottom_history -> replaceFragment(History())
+                R.id.bottom_map -> replaceFragment(Map())
+                R.id.bottom_settings -> replaceFragment(Settings())
 
-        bottomNavigationView.selectedItemId = R.id.bottom_scanner
+                else -> {
 
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_scanner -> {
-                    return@setOnItemSelectedListener true
                 }
-                R.id.bottom_history -> {
-                    startNewActivity(HistoryActivity::class.java)
-                    return@setOnItemSelectedListener true
-                }
-                R.id.bottom_map -> {
-                    startNewActivity(MapActivity::class.java)
-                    return@setOnItemSelectedListener true
-                }
-                R.id.bottom_settings -> {
-                    startNewActivity(SettingsActivity::class.java)
-                    return@setOnItemSelectedListener true
-                }
-                else -> false
             }
+            true
         }
+
+
     }
 
-    private fun startNewActivity(activityClass: Class<*>) {
-        val intent = Intent(this, activityClass)
-        startActivity(intent)
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
