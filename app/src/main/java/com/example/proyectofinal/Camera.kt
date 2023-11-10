132e04564d3aafc528e9e90e5d46031f5f769599
Roberto Leija <77653902+roberto-leija@users.noreply.github.com>
2023-11-10 01:20:18 -0600

    added scanner
    scanner implementation, missing connection with API

--- app/src/main/java/com/example/proyectofinal/Camera.kt
+++ app/src/main/java/com/example/proyectofinal/Camera.kt
@@ -1,59 +1,50 @@
 package com.example.proyectofinal
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Camera.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.zxing.integration.android.IntentIntegrator
+import com.example.proyectofinal.databinding.FragmentCameraBinding
 class Camera : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCameraBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Camera.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Camera().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnScan.setOnClickListener {
+            startScanner()
+        }
+    }
+
+    private fun startScanner() {
+        val scanner = IntentIntegrator.forSupportFragment(this)
+        scanner.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result.contents == null) {
+                Toast.makeText(requireContext(), "Cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Scanned: ${result.contents}", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
