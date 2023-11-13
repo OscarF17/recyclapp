ad7ebe3b6ad1b5881d99ed78270db49342935ba4
José Manuel <126370753+Josemgallegos@users.noreply.github.com>
2023-11-12 22:58:38 -0600

    changed action bar

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectofinal
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         // Iniciar con el fragmento de la cámara abierto
         replaceFragment(Camera())
